770c103e1c04e226865a5f875382a5f87c7501fd
Vinicius <88400874+viniciusparlatozaura@users.noreply.github.com>
2021-09-14 16:53:49 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um numero:'))
+n2 = int(input('numero outro numero:'))
+soma = (n1 + n2)
+
+print("A soma {} + {} é = {}".format(n1,n2,soma))
---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+nota1 = int(input('digite uma nota'))
+nota2 = int(input('digite uma nota'))
+nota3 = int(input('digite uma nota'))
+nota4 = int(input('digite uma nota'))
+media = (nota1 + nota2 + nota3 + nota4)/4
+print("a media das notas é {}".format(media))
